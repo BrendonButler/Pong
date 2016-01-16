94748e0a1b04d4edbf93d845cbf147a109d8b47f
Brendon Butler <bbutler452@gmail.com>
2016-01-15 23:57:41 -0500

    Initial commit

---
+++ src/net/sparkzz/pong/Pong.java
@@ -0,0 +1,11 @@
+package net.sparkzz.pong;
+
+/**
+ * Created by Brendon Butler on 1/15/2016.
+ */
+public class Pong {
+
+	public static void main(String[] args) {
+		System.out.println("Ping Pong!");
+	}
+}
